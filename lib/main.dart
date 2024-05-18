fc280a8494a5ca2f2701313ed700bee3458faf42
MohamedkamalSoudi <mk0875734@gmail.com>
2024-05-18 12:47:23 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp);}
+void runApp(Type weatherApp) {
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home:,
+    );
+  }
+}
+
+
