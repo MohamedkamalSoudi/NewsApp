7e531cf35f855883e6961d070b054c24260b4463
MohamedkamalSoudi <mk0875734@gmail.com>
2024-05-21 17:25:49 +0300

    ScreenUtilInit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/pages/home_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
- runApp( WeatherApp()); 
-}
+void main() => runApp( 
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => WeatherApp(), // Wrap your app
+  ),
+);
 
 class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child:  MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        home: HomeView(),
+      ),
     );
   }
 }
